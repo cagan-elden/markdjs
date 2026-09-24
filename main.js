8a5e6ac6740f77dcade73e146d94b5561ce52629
Çağan Eldenk <151869643+cagan-elden@users.noreply.github.com>
2026-09-24 21:14:40 +0300

    create rendmd script

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// this function uses regex to render html elements by markdown
 const inlineRegexParser = (line) => {
     return (line
         .replace(/`([^`]+)`/g, '<code>$1</code>')
@@ -6,3 +7,33 @@ const inlineRegexParser = (line) => {
         .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>')
     );
 };
+
+const rendMD = (mdString) => {
+    const lineArr = mdString.split(/\r\n?\n/);
+    const renderedArr = [];
+
+    let listChecker = false;
+
+    for (let i=0; i < lineArr.length; i++) {
+        const line = lineArr[i];
+        const lineNoSpaces = line.trim();
+
+        // check for list tags
+        if (lineNoSpaces.startsWith('- ')) {
+            if (!listChecker) { renderedArr.push('<ul>'); listChecker = true; }
+            renderedArr.push(`<li>${ inlineRegexParser(line.slice(2)) }</li>`);
+        } else {
+            if (listChecker) { renderedArr.push('</ul>'); listChecker = false; }
+        }
+
+        // check for heading tags
+        if (lineNoSpaces.startsWith('# ')) { renderedArr.push(`<h1>${ inlineRegexParser(lineNoSpaces.slice(2)) }</h1>`); }
+        else if (lineNoSpaces.startsWith('## ')) { renderedArr.push(`<h2>${ inlineRegexParser(lineNoSpaces.slice(3)) }</h2>`); }
+        else if (lineNoSpaces.startsWith('### ')) { renderedArr.push(`<h3>${ inlineRegexParser(lineNoSpaces.slice(4)) }</h3>`); }
+
+        // create your own css for horizontal lines with this class
+        else if (lineNoSpaces.startsWith('---')) { renderedArr.push('<div class="md-line"></div>') }
+
+        else { renderedArr.push(`<p>${ inlineRegexParser(lineNoSpaces) }</p>`) }
+    }
+}
