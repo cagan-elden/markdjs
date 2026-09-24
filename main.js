d15001ba0340e8ebe698105a97bd11a2bd5aad98
Çağan Eldenk <151869643+cagan-elden@users.noreply.github.com>
2026-09-24 20:45:50 +0300

    create regex parser

---
+++ main.js
@@ -0,0 +1,8 @@
+const inlineRegexParser = (line) => {
+    return (line
+        .replace(/`([^`]+)`/g, '<code>$1</code>')
+        .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')
+        .replace(/\*([^\*]+)\*/g, '<em>$1</em>')
+        .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>')
+    );
+};
